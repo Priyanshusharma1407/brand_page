b10f8bf4d658a9f564fc01ee8b4cac5f5c3dfe75
Priyanshusharma1407 <priyanshusharma14072003@gmail.com>
2023-06-30 20:00:22 +0530

    second commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+function MainSection()
+{
+    return(
+       <main className="site">
+      <div className="main-content">
+        <h1>YOUR FEET DESERVE BETTER</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+           
+           <div className="site-btn">
+        <button>SHOP NOW</button>
+        <button>CATEGORY</button>
+        </div>
+      </div>
+      <div className="main-image">
+       <img src="./images/shoe_image.png"/>
+      </div>
+       </main> 
+    )
+}
+export default MainSection;
